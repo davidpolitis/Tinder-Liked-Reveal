f7634207711bb8f79247f14ff5a572fa8ef540bb
David Politis <davidpolitis@users.noreply.github.com>
2020-03-04 10:08:37 +1100

    Fixed UserScript lines alignment

--- Tinder Liked Reveal.user.js
+++ Tinder Liked Reveal.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Tinder Liked Reveal
 // @namespace    https://davidpolitis.net/
-// @author    	 David Politis
-// @version		 1.0
-// @date		 2019-08-08
+// @author       David Politis
+// @version      1.0
+// @date         2019-08-08
 // @description  Reveals the first photo of people who have liked you without having premium membership.
-// @match      	 https://tinder.com/*
-// @copyright	 2019, David Politis
+// @match        https://tinder.com/*
+// @copyright    2019, David Politis
 // ==/UserScript==
 
 (function() {
